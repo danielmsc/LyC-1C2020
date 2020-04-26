6dd610560707f710becd8b351ef3f2aa75086d4f
Daniel Massaccisi <massaccisidaniel@gmail.com>
2020-04-26 13:02:46 -0300

    Eliminación de comillas en TS y gitignore

--- Entrega 1/Genera.bat
+++ Entrega 1/Genera.bat
@@ -2,7 +2,12 @@ flex lexico.l
 pause
 bison -dyv sintactico.y
 pause
-gcc lex.yy.c y.tab.c -o ejercicio.exe
+gcc lex.yy.c y.tab.c -o compilador.exe
 pause
-.\ejercicio.exe prueba.txt
+.\compilador.exe prueba.txt
+pause
+del lex.yy.c
+del y.tab.c
+del y.output
+del y.tab.h
 pause
